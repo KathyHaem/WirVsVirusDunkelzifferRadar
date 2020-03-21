4373b333f2236f00d426376b527467c7c3347bfd
chris schaller <chris.schaller@posteo.de>
2020-03-21 12:14:00 +0100

    add hello_world route to test communication of front and backend

--- backend/backend/api/routes.py
+++ backend/backend/api/routes.py
@@ -1,2 +1,13 @@
-from connexion import NoContent
+""" Module holding all routes defined in ../app.yml """
 
+def hello_world():
+    """
+    Returns mock data.
+
+    Returns:
+        dict: mock data.
+    """
+    return {
+        "test_id": 1,
+        "message": "hello world"
+    }
