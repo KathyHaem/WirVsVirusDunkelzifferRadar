50541e08c55d61cd0092961f0587633ab6ce433a
Sarina <meyer@cl.uni-heidelberg.de>
2020-03-21 18:39:59 +0100

    Changed API query and added insertion into database

--- external_data.py
+++ external_data.py
@@ -2,42 +2,61 @@ import pandas as pd
 import requests
 from collections import defaultdict
 import datetime as dt
+from arcgis.features import Table
+import psycopg2
 
-#from arcgis.gis import GIS
 
-#gis = GIS()
-
-#landkreise = gis.content.search(query='title:RKI_Landkreisdaten', outside_org=True)
-#print(landkreise)
-
-def get_current_data():
-    return _request_external_data()
+def get_current_data(setup=False):
+    data = _request_external_data()
+    return data
 
 def _request_external_data(date=None):
     if not date:
         date = dt.datetime.now().strftime('%Y-%m-%d')
-    query = {'f': 'json',
-             'where': "Meldedatum<'{}'".format(date),
-             'returnGeometry': False,
-             'spatialRel': 'esriSpatialRelIntersects',
-             'outFields': 'Bundesland,Landkreis,Altersgruppe,Geschlecht,AnzahlFall,AnzahlTodesfall,'
-                          'Meldedatum',
-             'orderByFields': 'Meldedatum asc',
-             'resultOffset': 0,
-             'resultRecordCount': 20000,
-             'cacheHint': True}
-    r = requests.get(
-        'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19'
-        '/FeatureServer/0/query',
-        params=query)
 
-    raw_data = r.json()
-    print(raw_data)
+    table = Table("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0")
+    raw_data = table.query(where="Meldedatum<'{}'".format(date),
+                           out_fields='Bundesland,Landkreis,Altersgruppe,Geschlecht,AnzahlFall,'
+                                        'AnzahlTodesfall,Meldedatum')
+
     data = defaultdict(list)
-    for object in raw_data['features']:
-        for k, v in object['attributes'].items():
+    for object in raw_data.features:
+        for k, v in object.attributes.items():
+            if k == 'Meldedatum':
+                v = dt.date.fromtimestamp(v//1000)
             data[k].append(v)
+
 
     return pd.DataFrame(data)
 
-#print(get_current_data().to_string())
+def _setup_database(cursor):
+    cursor.execute('CREATE TABLE rki_covid19 ('
+                   'ObjectId integer PRIMARY KEY, '
+                   'Bundesland varchar, '
+                   'Landkreis varchar, '
+                   'Altersgruppe varchar, '
+                   'Geschlecht varchar, '
+                   'AnzahlFall integer, '
+                   'AnzahlTodesfall integer, '
+                   'Meldedatum date);')
+
+def _get_last_report_date(cursor):
+    cursor.execute('SELECT MAX(Meldedatum) FROM rki_covid19')
+    return cursor.fetchone()[0]
+
+def _get_current_entries(cursor):
+    cursor.execute('SELECT ObjectId FROM rki_covid19')
+    return [result[0] for result in cursor.fetchall()]
+
+def _store_to_database(cursor, data):
+    rows = data.to_dict(orient='records')
+    existing_entries = _get_current_entries(cursor)
+    for row in rows:
+        if row['ObjectId'] not in existing_entries:
+            meldedatum = dt.date.fromtimestamp(row['Meldedatum']//1000)
+            cursor.execute('INSERT INTO rki_covid19 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                           (row['ObjectId'], row['Bundesland'], row['Landkreis'],
+                            row['Altersgruppe'], row['Geschlecht'], row['AnzahlFall'],
+                            row['AnzahlTodesfall'], meldedatum))
+
+
