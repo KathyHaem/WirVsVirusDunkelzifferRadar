f21772d188b9ba3140faa154ca7d674d569926b3
Philipp <p.t.h@posteo.de>
2020-03-22 12:05:10 +0100

    Changed external_data To Get Access To Aggregated Table

--- worker/analysis.py
+++ worker/analysis.py
@@ -1,9 +1,17 @@
 #from connection import get_database_table as get_table
 #from connection import pd
-from external_data import pd
-from external_data import get_current_data
+from dotenv import load_dotenv
+from external_data import pd, psycopg2, os
+from external_data import _get_data_from_database, _store_to_database_aggregated
 
-df_rki_covid19 = get_current_data()
+load_dotenv()
+conn = psycopg2.connect(dbname=os.environ.get('DB_NAME'), user=os.environ.get('DB_USER'),
+                        password=os.environ.get('DB_PASSWORD'), host=os.environ.get('DB_HOST'),
+                        port=os.environ.get('DB_PORT'), sslcert=os.environ.get('DB_SSL_CERT'),
+                        sslkey=os.environ.get('DB_SSL_KEY'))
+cursor = conn.cursor()
+
+df_rki_covid19 = _get_data_from_database(cursor)
 #df_rki_covid19.to_csv('data.csv', index=False)
 
 #df_rki_covid19 = pd.read_csv("data.csv")
@@ -25,3 +33,8 @@ print(
         & (df_rki_covid19.Landkreis == "SK Offenbach")
     ]
 )
+
+_store_to_database_aggregated(df_rki_covid19, cursor)
+
+conn.close()
+
