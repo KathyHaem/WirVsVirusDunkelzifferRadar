70304788a60f3054011233af139c4a85cb3838a1
Sarina <meyer@cl.uni-heidelberg.de>
2020-03-22 10:30:05 +0100

    Added database insertion of questionnaire data

--- backend/backend/routes.py
+++ backend/backend/routes.py
@@ -1,4 +1,13 @@
 """ Module holding all routes defined in app.yml """
+import os
+import datetime as dt
+
+from .db_connector import DBConnector
+
+DB = DBConnector(db_name=os.environ.get('DB_NAME'), db_user=os.environ.get('DB_USER'),
+                 db_password=os.environ.get('DB_PASSWORD'), db_host=os.environ.get('DB_HOST'),
+                 db_port=os.environ.get('DB_PORT'), db_ssl_cert=os.environ.get('DB_SSL_CERT'),
+                 db_ssl_key=os.environ.get('DB_SSL_KEY'))
 
 def hello_world():
     """
@@ -22,6 +31,10 @@ def add_questionnaire_entry(body):
     Returns:
         tuple: status message.
     """
+    if body:
+        if "corona_date" in body:
+            body['corona_date'] = dt.datetime.fromtimestamp(body['corona_date'])
+        DB.insert_row(body)
     if body.get("corona_positive"):
         return (
             {
