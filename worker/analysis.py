4a3c3f26157f002eea9cfeb7114d84799dfb46d7
KeBachmann <kevin.bachbold@gmail.com>
2020-03-22 16:01:56 +0100

    wrapper finished, dict to do

--- worker/analysis.py
+++ worker/analysis.py
@@ -14,27 +14,13 @@ def exponential_model(x, a, b, c):
     # return a*np.exp(b*(x-c))
     return a * b ** (x + c)
 
-# Loading Dataframe
-df_rki_covid19_aggregated = getAggregatedTable()
 
+def get_dunkelziffer_bundesland(df_rki_covid19_aggregated, bundesland):
 
-# Dataframe manipulations
-df_rki_covid19_aggregated.MELDEDATUM = pd.to_datetime(
-    df_rki_covid19_aggregated.MELDEDATUM
-)
-
-df_rki_covid19_aggregated["tage"] = (
-    df_rki_covid19_aggregated.MELDEDATUM
-    - df_rki_covid19_aggregated.MELDEDATUM.min()
-)
-
-laender = np.unique(df_rki_covid19_aggregated.BUNDESLAND.values)
-
-for land in laender:
     df_rki_covid19_aggregated = (
         df_rki_covid19_aggregated.groupby(["BUNDESLAND", "tage"])
         .sum()
-        .loc[land]
+        .loc[bundesland]
         .cumsum()
         .resample("1d")
         .ffill()
@@ -53,7 +39,7 @@ for land in laender:
     a = df_rki_covid19_aggregated.FALL_COUNT[0]
     country_coef = [2.5, 3, 3.5]
     time_delay = np.arange(8, 13, 1)
-    growth_rate = np.arange(1.15, 1.25, 0.01)
+    growth_rate = np.arange(1.15, 1.20, 0.01)
 
     adam = np.zeros(
         (
@@ -68,8 +54,87 @@ for land in laender:
         for t, days in enumerate(time_delay):
             for b, rate in enumerate(growth_rate):
                 adam[c, t, b, :] = exponential_model(x, coef * a, rate, days)
+    
     bob = np.mean(adam, axis=(0, 1, 2))
-    lb = np.percentile(adam, 0.25, axis=(0, 1, 2))
-    ub = np.percentile(adam, 0.75, axis=(0, 1, 2))
+    #lb = np.percentile(adam, 0.25, axis=(0, 1, 2))
+    #ub = np.percentile(adam, 0.75, axis=(0, 1, 2))
 
     print(bob)
+    dictionary = bob
+    return dictionary
+
+
+def get_dunkelziffer_landkreis(df_rki_covid19_aggregated, kreis):
+
+    df_rki_covid19_aggregated = (
+        df_rki_covid19_aggregated.groupby(["LANDKREIS", "tage"])
+        .sum()
+        .loc[kreis]
+        .cumsum()
+        .resample("1d")
+        .ffill()
+    )
+
+    df_rki_covid19_aggregated = df_rki_covid19_aggregated.reset_index()
+
+    df_rki_covid19_aggregated.tage = (
+        df_rki_covid19_aggregated.tage / np.timedelta64(1, "D")
+    )
+
+    # Creating "predictions"
+    x = np.linspace(
+        0, len(df_rki_covid19_aggregated.tage), len(df_rki_covid19_aggregated.tage)
+    )
+    a = df_rki_covid19_aggregated.FALL_COUNT[0]
+    country_coef = [2.5, 3, 3.5]
+    time_delay = np.arange(8, 13, 1)
+    growth_rate = np.arange(1.15, 1.20, 0.01)
+
+    adam = np.zeros(
+        (
+            len(country_coef),
+            len(time_delay),
+            len(growth_rate),
+            len(df_rki_covid19_aggregated.tage),
+        )
+    )
+
+    for c, coef in enumerate(country_coef):
+        for t, days in enumerate(time_delay):
+            for b, rate in enumerate(growth_rate):
+                adam[c, t, b, :] = exponential_model(x, coef * a, rate, days)
+    
+    bob = np.mean(adam, axis=(0, 1, 2))
+    #lb = np.percentile(adam, 0.25, axis=(0, 1, 2))
+    #ub = np.percentile(adam, 0.75, axis=(0, 1, 2))
+
+    print(bob)
+    dictionary = bob
+    return dictionary
+
+# Loading Dataframe
+df_rki_covid19_aggregated = getAggregatedTable()
+df_rki_covid19_aggregated=df_rki_covid19_aggregated[df_rki_covid19_aggregated.BUNDESLAND!='-nicht erhoben-']
+df_rki_covid19_aggregated=df_rki_covid19_aggregated[df_rki_covid19_aggregated.LANDKREIS !='-nicht erhoben-']
+
+# Dataframe manipulations
+df_rki_covid19_aggregated.MELDEDATUM = pd.to_datetime(
+    df_rki_covid19_aggregated.MELDEDATUM
+)
+
+df_rki_covid19_aggregated["tage"] = (
+    df_rki_covid19_aggregated.MELDEDATUM
+    - df_rki_covid19_aggregated.MELDEDATUM.min()
+)
+
+for land in np.unique(df_rki_covid19_aggregated.BUNDESLAND.values):
+    dictionary = get_dunkelziffer_bundesland(df_rki_covid19_aggregated, land)
+    #to do
+    #function to push to datenbank
+    
+for kreis in np.unique(df_rki_covid19_aggregated.LANDKREIS.values):
+    dictionary = get_dunkelziffer_landkreis(df_rki_covid19_aggregated, kreis)
+    #to do
+    #function to push to datenbank
+
+    
