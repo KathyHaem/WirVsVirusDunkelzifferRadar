61f2505c1a5a3e245cf316e9684f5860ef56ab5b
chris schaller <chris.schaller@posteo.de>
2020-03-22 14:45:40 +0100

    fix db_connector.py

--- backend/backend/db_connector.py
+++ backend/backend/db_connector.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class DBConnector:
 
-    def __init__(self, db_name, db_host, db_port, db_user, db_password,db_ssl_cert, db_ssl_key):
+    def __init__(self, db_name, db_host, db_port, db_user, db_password, db_ssl_cert, db_ssl_key):
         """
         Class for connecting to the dunkelziffer_radar database for backend purposes.
 
@@ -22,8 +22,8 @@ class DBConnector:
         self.cursor = self.conn.cursor()
 
         # check if table exists. If it does not exist, a new table is created
-        self.cursor.execute('SELECT EXISTS(SELECT * FROM information_schema.tables '
-                            'WHERE table_name=dunkelziffer_radar)')
+        self.cursor.execute("SELECT EXISTS(SELECT * FROM information_schema.tables "
+                            "WHERE table_name='dunkelziffer_radar')")
         if not self.cursor.fetchone()[0]:
             self._setup_table()
 
@@ -33,36 +33,38 @@ class DBConnector:
 
         :return: None
         """
-        #TODO: Missing ID?
-        self.cursor.execute('CREATE TABLE dunkelziffer_radar ('
-                            'first_time BOOLEAN'
-                            'gender VARCHAR'
-                            'age INTEGER'
-                            'zip_code INTEGER'
-                            'has_cough BOOLEAN'
-                            'cough_dry BOOLEAN'
-                            'cough_productive BOOLEAN'
-                            'cough_painful BOOLEAN'
-                            'fever BOOLEAN'
-                            'fever_suspected BOOLEAN'
-                            'fever_confirmed BOOLEAN'
-                            'nose_affected BOOLEAN'
-                            'pain BOOLEAN'
-                            'pain_head BOOLEAN'
-                            'pain_limbs BOOLEAN'
-                            'diarrhea BOOLEAN'
-                            'throat BOOLEAN'
-                            'dyspnea BOOLEAN'
-                            'fatigue BOOLEAN'
-                            'corona_test BOOLEAN'
-                            'corona_positive BOOLEAN'
-                            'corona_date DATE'
-                            'pre_illness VARCHAR'
-                            'asthma BOOLEAN'
-                            'allergy BOOLEAN'
-                            'blood_pressure BOOLEAN'
-                            'diabetes BOOLEAN'
-                            'other_illness BOOLEAN);')
+        # TODO: Missing ID?
+        self.cursor.execute("CREATE TABLE dunkelziffer_radar ("
+                            "uuid UUID, "
+                            "entry_date DATE, "
+                            "first_time BOOLEAN, "
+                            "gender VARCHAR, "
+                            "age INTEGER, "
+                            "zip_code INTEGER, "
+                            "cough BOOLEAN, "
+                            "cough_dry BOOLEAN, "
+                            "cough_productive BOOLEAN, "
+                            "cough_painful BOOLEAN, "
+                            "fever BOOLEAN, "
+                            "fever_suspected BOOLEAN, "
+                            "fever_confirmed BOOLEAN, "
+                            "nose_affected BOOLEAN, "
+                            "pain BOOLEAN, "
+                            "pain_head BOOLEAN, "
+                            "pain_limbs BOOLEAN, "
+                            "diarrhea BOOLEAN, "
+                            "throat BOOLEAN, "
+                            "dyspnea BOOLEAN, "
+                            "fatigue BOOLEAN, "
+                            "corona_test BOOLEAN, "
+                            "corona_positive BOOLEAN, "
+                            "corona_date DATE, "
+                            "pre_illness VARCHAR, "
+                            "asthma BOOLEAN, "
+                            "allergy BOOLEAN, "
+                            "blood_pressure BOOLEAN, "
+                            "diabetes BOOLEAN, "
+                            "other_illness BOOLEAN);")
         self.conn.commit()
 
     def close_connection(self):
@@ -84,8 +86,8 @@ class DBConnector:
         """
         fields, values = zip(*data.items())
         n = len(values)
-        query = 'INSERT INTO dunkelziffer_radar ({0}) VALUES ({1})'.format(', '.join(fields),
-                                                                           ', '.join(['%s']*n))
+        query = "INSERT INTO dunkelziffer_radar ({0}) VALUES ({1})".format(", ".join(fields),
+                                                                           ", ".join(["%s"] * n))
         self.cursor.execute(query, values)
         self.conn.commit()
 
@@ -95,7 +97,7 @@ class DBConnector:
 
         :return: pandas.DataFrame: complete data of the dunkelziffer_radar table.
         """
-        self.cursor.execute('SELECT * FROM dunkelziffer_radar')
+        self.cursor.execute("SELECT * FROM dunkelziffer_radar")
         data = self.cursor.fetchall()
         columns = [desc[0] for desc in self.cursor.description]
         return pd.DataFrame(data, columns=columns)
