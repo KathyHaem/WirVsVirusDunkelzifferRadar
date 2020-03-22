3825a779b94960c09f9731ac5ef113768b76449b
Janis Meyer <janis_meyer@outlook.de>
2020-03-22 04:03:03 +0100

    add another layer to the map.

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -9,16 +9,6 @@ import Drilldown from 'highcharts/modules/drilldown';
 Drilldown(Highcharts);
 
 const germany = require('src/assets/plz-1stellig.geo.json');
-const filtered0 = require('src/assets/filtered-0.geo.json');
-const filtered1 = require('src/assets/filtered-1.geo.json');
-const filtered2 = require('src/assets/filtered-2.geo.json');
-const filtered3 = require('src/assets/filtered-3.geo.json');
-const filtered4 = require('src/assets/filtered-4.geo.json');
-const filtered5 = require('src/assets/filtered-5.geo.json');
-const filtered6 = require('src/assets/filtered-6.geo.json');
-const filtered7 = require('src/assets/filtered-7.geo.json');
-const filtered8 = require('src/assets/filtered-8.geo.json');
-const filtered9 = require('src/assets/filtered-9.geo.json');
 MapModule(Highcharts);
 
 @Component({
@@ -27,19 +17,6 @@ MapModule(Highcharts);
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-
-    filter(id: string) {
-      const regions = germany.features;
-      const filteredData = [];
-      regions.forEach(element => {
-        console.log("2");
-        if(element.properties['id'] == id)
-          filteredData.push(element);
-      });
-      
-      return filteredData;
-    };
-
     // Initiate the chart
     chartMap: Highcharts.Options = {
       chart: {
@@ -51,7 +28,46 @@ export class MapComponent {
       colorAxis: {
         min: 0
       },
-      series: [{
+      series: [],
+      drilldown:  {
+          series: []
+      }    
+    };
+
+    constructor() { 
+      let stage = 0;
+      let id = '';
+      let data = [];
+      let drilldown = [];
+      for(let i = 0; i < 10; i++) {
+        let s = (stage * 10 + i).toString();
+        data.push({'value': i, 'drilldown': s, 'id': s});
+
+        let drilldownData = [];
+        for(let j = 0; j < 10; j++) {
+          let t = i.toString() + j.toString();
+          drilldownData.push({'id': t, 'value': i*10 + j, 'drilldown': t});
+        }
+
+        let mapData = require(`src/assets/${s}.geo.json`);
+        drilldown.push({ id: s,
+        type: 'map',
+        mapData: mapData,
+        data: drilldownData,
+        joinBy: 'id' });
+
+        for(let j = 0; j < 10; j++) {
+          let t = i.toString() + j.toString();
+          mapData = require(`src/assets/${t}.geo.json`);
+          drilldown.push({ id: t,
+          type: 'map',
+          mapData: mapData,
+          data: [{'id': t, 'value': i*10 + j}],
+          joinBy: 'id' });
+        }
+      }
+
+      this.chartMap.series.push({
         name: 'Random data',
         states: {
           hover: {
@@ -64,129 +80,15 @@ export class MapComponent {
         },
         allAreas: false,
         type: 'map',
-        data: [
-          { 'name' : '0',
-            'value': 123,
-            'drilldown': '0',
-            'id': '0'},
-          { 'name' : '1',
-            'value': 123,
-            'drilldown': '1',
-            'id': '1'},
-          { 'name' : '2',
-            'value': 123,
-            'drilldown': '2',
-            'id': '2'},
-          { 'name' : '3',
-            'value': 123,
-            'drilldown': '3',
-            'id': '3'},
-          { 'name' : '4',
-            'value': 123,
-            'drilldown': '4',
-            'id': '4'},
-          { 'name' : '5',
-            'value': 123,
-            'drilldown': '5',
-            'id': '5'},
-          { 'name' : '6',
-            'value': 123,
-            'drilldown': '6',
-            'id': '6'},
-          { 'name' : '7',
-            'value': 123,
-            'drilldown': '7',
-            'id': '7'},
-          { 'name' : '8',
-            'value': 123,
-            'drilldown': '8',
-            'id': '8'},
-          { 'name' : '9',
-            'value': 123,
-            'drilldown': '9',
-            'id': '9'}
-        ], joinBy: 'id'
-      }],
-      drilldown:  {
-          series: [
-            { id: '0',
-              type: 'map',
-              mapData: filtered0,
-              data: [
-                {'name': '0', 'id': '0', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '1',
-              type: 'map',
-              mapData: filtered1,
-              data: [
-                {'name': '1', 'id': '1', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '2',
-              type: 'map',
-              mapData: filtered2,
-              data: [
-                {'name': '2', 'id': '2', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '3',
-              type: 'map',
-              mapData: filtered3,
-              data: [
-                {'name': '3', 'id': '3', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '4',
-              type: 'map',
-              mapData: filtered4,
-              data: [
-                {'name': '4', 'id': '4', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '5',
-              mapData: filtered5,
-              type: 'map',
-              data: [
-                {'name': '5', 'id': '5', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '6',
-              mapData: filtered6,
-              type: 'map',
-              data: [
-                {'name': '6', 'id': '6', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '7',
-              mapData: filtered7,
-              type: 'map',
-              data: [
-                {'name': '7', 'id': '7', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '8',
-              mapData: filtered8,
-              type: 'map',
-              data: [
-                {'name': '8', 'id': '8', 'value': 222}
-              ],
-              joinBy: 'id' },
-              { id: '9',
-              mapData: filtered9,
-              type: 'map',
-              data: [
-                {'name': '9', 'id': '9', 'value': 222}
-              ],
-              joinBy: 'id' }
-          ]
-      }
-      
-    };
+        data: data, 
+        joinBy: 'id'
+      });
 
-    constructor() { }
+      this.chartMap.drilldown.series = drilldown;
+    }
 
     ngOnInit(): void {
+
       Highcharts.mapChart('container', this.chartMap);
     }
 }
